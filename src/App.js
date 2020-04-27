a1f5a3210131692a1f17af6fbac5306861ea3484
khaled mehrez <khaledmehrezsimplon@gmail.com>
2020-04-28 00:20:30 +0100

    add project to github

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import './App.css';
+
+
+
+
+class App extends React.Component{
+  constructor(props){
+      super(props);
+      this.state = {timeNumber : "" ,hour:0,minutes:0,secondes:0,starting:0};
+  }
+
+   handleChange=event => {
+    this.setState({timeNumber: event.target.value})
+  }
+  
+
+start=()=>{
+  
+ this.interval=setInterval(() => {this.setState({timeNumber:parseInt(this.state.timeNumber)+1})}
+
+   
+ , 1000);}
+
+ stop=()=>{
+   clearInterval(this.interval)
+ }
+        
+ reset=()=>{
+  this.setState({timeNumber:""})
+ }
+
+
+  
+ render(){
+     return(
+         <div className="timer">
+  <input type="text" id="fname" name="fname" value={this.state.timeNumber}   onChange={this.handleChange}/>
+  <button id="btn" type="button" onClick={this.start}    >start!</button>
+  <button id="btn" type="button" onClick={this.stop}    >stop!</button>
+  <button id="btn" type="button" onClick={this.reset}    >reset!</button>
+  <div className="paragraphe">
+  <p>hour={(Math.trunc(this.state.timeNumber/3600))}</p>
+  <p>minutes={Math.trunc(this.state.timeNumber/60)%60}</p>
+  <p>secondes={Math.trunc(this.state.timeNumber%60)}</p>
+  </div>
+  
+  </div>
+  )    
+ }
+}
+
+
+export default App;
